5f839daa0db61c8a0bc77bb6c859eb8c602ec941
Tony <115674436+tonyporsche123@users.noreply.github.com>
2022-12-29 11:40:16 +0000

    Update home.py

--- home.py
+++ home.py
@@ -10,25 +10,25 @@ import emoji
 def load_lottiefile(filepath: str):
       with open(filepath, "r") as f:
           return json.load(f)
-  def load_lottieurl(url: str):
-      r = requests.get(url)
-      if r.status_code != 200:
-          return None
-      return r.json()
-  st.write(emoji.emojize("""# PEM Bké """))
+def load_lottieurl(url: str):
+r = requests.get(url)
+if r.status_code != 200:
+    return None
+return r.json()
+st.write(emoji.emojize("""# PEM Bké """))
 
-  lottie_coding = load_lottiefile("welcome.json")
-  lottie_hello = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_69HH48.json")
+lottie_coding = load_lottiefile("welcome.json")
+lottie_hello = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_69HH48.json")
 
-  st_lottie(
-      lottie_hello,
-      speed=1,
-      reverse=False,
-      loop=True,
-      quality="low", # medium ; high
-      #renderer="svg", # canvas
-      height=None,
-      width=None,
-      key=None,
-  )
-  st.balloons()
+st_lottie(
+lottie_hello,
+speed=1,
+reverse=False,
+loop=True,
+quality="low", # medium ; high
+#renderer="svg", # canvas
+height=None,
+width=None,
+key=None,
+)
+st.balloons()
